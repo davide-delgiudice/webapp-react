d114d63538b2f3194e16d95e599a796e129dfc73
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-31 12:24:11 +0200

    [UPD] import homepage in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import DefaultLayout from "./layout/DefaultLayout"
+import Homepage from "./pages/Homepage"
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />}>
-            <Route index element={<h1>Homepage</h1>}></Route>
+            <Route index element={<Homepage />}></Route>
             <Route path="/books/:id" element={<h1>Dettaglio pagina</h1>}></Route>
           </Route>
         </Routes>
