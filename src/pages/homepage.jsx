07c49e2103b954df2b9edfa28885f9c6fd20d0e1
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-31 12:21:04 +0200

    [UPD] create pages folder and homepage jsx file

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const homepage = () => {
+  return (
+    <>
+        <h1 className='text-primary'>Bool Books</h1>
+        <h2>
+            <i>The nerdest book community</i>
+        </h2>
+        <div className='row'>
+            {/* inseriamo i libri */}
+        </div>
+    </>
+  )
+}
+
+export default homepage
