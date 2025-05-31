2530a2020aac4b7bf8725c4cc04580dde57978ca
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-31 12:08:54 +0200

    [UPD] import default layout, browser router, routes and route in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import DefaultLayout from "./layout/DefaultLayout"
 
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route index element={<h1>Homepage</h1>}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
