95e4b3dea426ba1ec21e4214ae5845a58e92e226
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-31 11:51:57 +0200

    [UPD] create layout folder and defaultlayout jsx file

---
+++ src/layout/DefaultLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DefaultLayout = () => {
+  return (
+    <>
+        <header></header>
+        <main className='container'>
+            {/* pagine */}
+        </main>
+    </>
+  )
+}
+
+export default DefaultLayout
