2b9cf53019214c807d886d19a897f74583bbb747
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-31 11:59:07 +0200

    [UPD] create components folder, header jsx file and import Link

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <nav className='navbar bg-primary mb-4'>
+        <div className='container-fluid'>
+            <Link className='navbar-brand' to="/">
+                Bool Books
+            </Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
