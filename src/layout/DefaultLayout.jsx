70a50697dedb616c81ecbc8e4fa386efaa460e67
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-31 12:08:22 +0200

    [UPD] import header in default layout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Header from '../components/Header'
 
 import { Outlet } from 'react-router-dom'
 
 const DefaultLayout = () => {
   return (
     <>
-        <header></header>
+        <Header />
         <main className='container'>
             <Outlet />
         </main>
