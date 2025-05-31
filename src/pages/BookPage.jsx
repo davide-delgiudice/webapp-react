12af1ca4cfd46d5f6b298b2431bb1bb7d47717a7
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-31 12:27:57 +0200

    [UPD] create book page jsx file in pages

---
+++ src/pages/BookPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BookPage = () => {
+  return (
+    <div className='row'>
+        <div className="col-12 col-md-6 col-lg-4">
+            Immagine
+        </div>
+        <div className="col-12 col-md-6 col-lg-4">
+            <h1>Titolo</h1>
+            <h2>altre informazioni</h2>
+        </div>
+    </div>
+  )
+}
+
+export default BookPage
