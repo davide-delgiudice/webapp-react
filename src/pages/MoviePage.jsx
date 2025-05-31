2a57e8d536eebf096ca22faa7268b821f711254f
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-31 14:37:18 +0200

    [UPD] add img and details in movie page

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -4,11 +4,17 @@ const BookPage = () => {
   return (
     <div className='row'>
         <div className="col-12 col-md-6 col-lg-4">
-            Immagine
+            <img src="https://picsum.photos/500/300" className='img-fluid' alt="img" />
         </div>
         <div className="col-12 col-md-6 col-lg-4">
             <h1>Titolo</h1>
-            <h2>altre informazioni</h2>
+            <h3 className='text-primary'>Titolo</h3>
+            <h4>
+              <em>Regista</em>
+            </h4>
+            <p>Genre</p>
+            <p>Anno di uscita</p>
+            <p>Sinossi</p>
         </div>
     </div>
   )
