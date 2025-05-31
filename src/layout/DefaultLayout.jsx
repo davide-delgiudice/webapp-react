747b6de65f6bd6f3e107814a9f7d6ae9c8743804
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-31 11:54:18 +0200

    [UPD] import outlet in layout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import { Outlet } from 'react-router-dom'
+
 const DefaultLayout = () => {
   return (
     <>
         <header></header>
         <main className='container'>
-            {/* pagine */}
+            <Outlet />
         </main>
     </>
   )
