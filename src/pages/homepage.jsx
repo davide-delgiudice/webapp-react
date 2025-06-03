1f3f1147b429ad6dec7946da5476398262ffd8af
davide-delgiudice <dav.del.giudice@gmail.com>
2025-06-03 17:53:13 +0200

    [UPD] delete array and add fetchMovies

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -4,58 +4,19 @@ import axios from 'axios'
 
 import { useState, useEffect } from 'react'
 
-const initialMovies = [
-  {
-    id: 1,
-    title: "titolo 1",
-    director: "regista 1",
-    genre: "genere 1",
-    release_year: "anno uscita 1",
-    image: "https://picsum.photos/500/300"
-  },
-  {
-    id: 2,
-    title: "titolo 2",
-    director: "regista 2",
-    genre: "genere 2",
-    release_year: "anno uscita 2",
-    image: "https://picsum.photos/500/300"
-  },
-  {
-    id: 3,
-    title: "titolo 3",
-    director: "regista 3",
-    genre: "genere 3",
-    release_year: "anno uscita 3",
-    image: "https://picsum.photos/500/300"
-  },
-  {
-    id: 4,
-    title: "titolo 4",
-    director: "regista 4",
-    genre: "genere 4",
-    release_year: "anno uscita 4",
-    image: "https://picsum.photos/500/300"
-  },
-  {
-    id: 5,
-    title: "titolo 5",
-    director: "regista 5",
-    genre: "genere 5",
-    release_year: "anno uscita 5",
-    image: "https://picsum.photos/500/300"
-  },
-]
-
 const homepage = () => {
-    const [movies, setMovies] = useState(initialMovies);
+    const [movies, setMovies] = useState([]);
 
-      useEffect(() => {
+      const fetchMovies = () => {
         axios.get('http://127.0.0.1:3000/movie').then((resp) => {
-          console.log(resp);
+          setMovies(resp.data);
         }).catch((err) => {
           console.log(err);
         });
+      }
+
+      useEffect(() => {
+        fetchMovies();
       }, []);
 
   return (
