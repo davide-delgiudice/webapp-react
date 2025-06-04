1e9138697f9ce9a109613fa7af916b50dede1da0
davide-delgiudice <dav.del.giudice@gmail.com>
2025-06-04 19:28:01 +0200

    [UPD] added handle submit on form and style classes

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -28,12 +28,12 @@ const ReviewForm = () => {
     };
 
   return (
-    <div className='card'>
+    <div className='card my-4'>
         <div className='card-header'>
             <h2>Aggiungi la tua recensione</h2>
         </div>
         <div className='card-body'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="" className="control-label">Name</label>
                     <input type="text" className="form-control" name="name" placeholder='Inserisci il tuo nome' value={formData.name} onChange={setFieldValue} required />
@@ -41,11 +41,11 @@ const ReviewForm = () => {
                     <input min="0" max="5" type="number" className="form-control" name="vote" placeholder='Inserisci il voto' value={formData.vote} onChange={setFieldValue} required />
                     <div className="form-group">
                         <label htmlFor="" className="control-label">Testo</label>
-                        <textarea name="text" id="text" className='form-comtrol' value={formData.text} onChange={setFieldValue}></textarea>
+                        <textarea name="text" id="text" className='form-control' value={formData.text} onChange={setFieldValue}></textarea>
                     </div>
                 </div>
-                <div className="form-group">
-                    <button className="btn-btn-main" type='submit'>Invia Recensione</button>
+                <div className="form-group mt-3">
+                    <button className="btn btn-primary" type='submit'>Invia Recensione</button>
                 </div>
             </form>
         </div>
