b2ed62c7399a4040daffd021746f8307e638acbf
davide-delgiudice <dav.del.giudice@gmail.com>
2025-06-04 18:51:45 +0200

    [UPD] create review form component with form and card classes

---
+++ src/components/ReviewForm.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const ReviewForm = () => {
+  return (
+    <div className='card'>
+        <div className='card-header'>
+            <h2>Aggiungi la tua recensione</h2>
+        </div>
+        <div className='card-body'>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="" className="control-label">Name</label>
+                    <input type="text" className="form-control" name="name" placeholder='Inserisci il tuo nome' required />
+                    <label htmlFor="" className="control-label">Voto</label>
+                    <input min="0" max="5" type="number" className="form-control" name="vote" placeholder='Inserisci il voto' required />
+                    <div className="form-group">
+                        <label htmlFor="" className="control-label">Testo</label>
+                        <textarea name="text" id="text" className='form-comtrol'></textarea>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default ReviewForm
